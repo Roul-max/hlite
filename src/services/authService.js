b7e4f327ca03a809a7bceff3bbcd7a0d8eed47b2
Roul-max <rohitkumarrrx@gmail.com>
2025-08-30 17:25:03 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,16 @@
-import api from "../api"; // this is your axios instance (api.js)
+import api from "./api";
 
+// Login request
 export const login = async (email, password) => {
   return api.post("/auth/login", { email, password });
 };
 
+// Signup request
 export const signup = async (name, email, password) => {
   return api.post("/auth/signup", { name, email, password });
 };
 
+// Verify OTP
 export const verifyOtp = async (email, otp) => {
   return api.post("/auth/verify-otp", { email, otp });
 };
