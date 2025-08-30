96fc6c09b0a653d394919a99ba99343ef4483eaa
Roul-max <rohitkumarrrx@gmail.com>
2025-08-30 15:24:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-const API_URL = import.meta.env.VITE_API_URL;
-s
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default ({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  })
+}
