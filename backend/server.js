bf9d1b6bd21d60fbe4a039cf8975a45eda5b1d08
Roul-max <rohitkumarrrx@gmail.com>
2025-08-30 14:50:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 // Security middleware
 app.use(helmet());
